e5a1a4dc9d2db051e6e86a9873b09a536ed87057
Jim Gregor <jimmyg0801@gmail.com>
2019-04-16 11:56:50 -0400

    These are the files that use gulp 4 and work OK so far.

---
+++ gulpfile-v4.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task("hello", function(done) { console.log("Hello!"); done(); });
+
+gulp.task('default', function() {
+	console.log("Hooray - you created a gulp task");
+});
+
+gulp.task('html', function(done) {
+	console.log("Imagine something useful being done to your HTML here");
+	done();
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+// Not sure if I should use gulp.series or gulp.parallel
+
+gulp.task('watch', function() {
+	console.log("Hooray - gulp 4 task watching!");
+	gulp.watch('./app/index.html', gulp.series('html'));
+	gulp.watch('./app/assets/styles/**/*.css', gulp.series('styles'));
+});
