e39518d8b26339bb37d6bbe681bb0edb5b50c73a
Jim Gregor <jimmyg0801@gmail.com>
2019-04-17 07:05:47 -0400

    Added new changes for gulp version 4 testing

--- gulpfile-v4.js
+++ gulpfile-v4.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task("hello", function(done) { console.log("Hello!"); done(); });
 
@@ -18,7 +19,7 @@ gulp.task('html', function(done) {
 
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars, nested, autoprefixer]))
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
